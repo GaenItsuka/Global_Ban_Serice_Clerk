06b8827394e8c4babd79fa72de183ae738a91152
GaenItsuka <motonobu.wiki@gmail.com>
2023-02-07 15:30:08 +0000

    feat: __main__.py

---
+++ Global_Ban_Serice_Clerk/__main__.py
@@ -0,0 +1,83 @@
+import os
+import logging
+import click
+import sys
+from Global_Ban_Serice_Clerk import (
+    message,
+    command,
+    callbackQuery,
+    utils,
+)
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ConversationHandler,
+    MessageHandler,
+    CallbackQueryHandler,
+    filters,
+)
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option(
+    "--ownerID", "ownerID", required=True, type=str, help="The owner of this bot."
+)
+def main(ownerID) -> None:
+    GBBTYPE, EVIDENCE, EVIDENCE_PHOTO = range(3)
+
+    logger.info("Running Pre-Flight setup.")
+    utils.preflightCheck(ownerID)
+
+    """Run the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(os.environ["BOT_TOKEN"]).build()
+
+    application.add_handler(CommandHandler("start", command.start))
+    application.add_handler(CommandHandler("report", command.report))
+    application.add_handler(CommandHandler("setAdmin", command.setAdmin))
+    application.add_handler(CommandHandler("revokeAdmin", command.revokeAdmin))
+    application.add_handler(CommandHandler("isAdmin", command.isAdmin))
+    application.add_handler(CommandHandler("set_headquarter", command.setHeadquarter))
+    application.add_handler(
+        CommandHandler("showRemainRequest", command.showRemainRequest)
+    )
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("submit", command.submit)],
+        states={
+            GBBTYPE: [
+                MessageHandler(filters.Regex("^(Spam|Harassment)$"), message.gbbtype)
+            ],
+            EVIDENCE: [MessageHandler(filters.TEXT, message.evidenceText)],
+            EVIDENCE_PHOTO: [
+                MessageHandler(filters.PHOTO, message.evidencePhoto),
+                CommandHandler("skip", message.skip_photo),
+            ],
+        },
+        fallbacks=[CommandHandler("cancel", message.cancel)],
+        # entry_points=[],
+    )
+    application.add_handler(
+        CallbackQueryHandler(
+            callbackQuery.button, pattern="^(processed|rejected)_\w{16}"
+        )
+    )
+    application.add_handler(
+        CallbackQueryHandler(
+            callbackQuery.buttonForReport, pattern="^(processed|rejected)_\-\d.*_\d.*$"
+        )
+    )
+
+    application.add_handler(conv_handler)
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
