eea387b84eb0e7176577c8b9f268f5463ca4fad2
GaenItsuka <motonobu.wiki@gmail.com>
2023-02-07 15:29:01 +0000

    feat: add __init__.py.

---
+++ Global_Ban_Serice_Clerk/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.8.0"
+# from command import *
+# from callbackQuery import *
+# from message import *
+# from utils import *
+
+from . import (
+    command,
+    callbackQuery,
+    message,
+    utils,
+)
