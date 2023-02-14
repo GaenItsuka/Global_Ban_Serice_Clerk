1591a1fe5ce207a54484ea1d6500dc5d3eb46d91
GaenItsuka <motonobu.wiki@gmail.com>
2023-02-14 01:02:48 +0000

    feat: add 'test' button.

--- Global_Ban_Service_Clerk/command.py
+++ Global_Ban_Service_Clerk/command.py
@@ -228,7 +228,7 @@ async def submit(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         ticketMessageID=chat.id,
     )
 
-    reply_keyboard = [["Spam", "Harassment"]]
+    reply_keyboard = [["Spam", "Harassment", "test"]]
 
     submitResponse = (
         "Hi! Please choose the type of gbb you want to submit.\n"
