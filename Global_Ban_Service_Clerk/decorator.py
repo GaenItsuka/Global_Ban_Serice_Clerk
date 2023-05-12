51937966d168ff2ab2471f96e0e8f00582a74b75
Gaenitsuka <Motonobu.wiki@gmail.com>
2023-05-12 15:16:17 +0000

    feat: implement more decorators to remove duplicated codes.

--- Global_Ban_Service_Clerk/decorator.py
+++ Global_Ban_Service_Clerk/decorator.py
@@ -1,6 +1,57 @@
+import logging
 from dotenv import dotenv_values
-from .utils import getHQIndex, checkGbbConfig
+from .utils import getHQIndex, checkGbbConfig, checkIsAdmin, checkIsVIP
 
+logger = logging.getLogger(__name__)
+
+def checkAdmin(func):
+    async def wraps(*args, **kargs):
+        user = args[0].effective_user
+        
+        if checkIsAdmin(user.id):
+            logger.info(
+                f"Admin {user.full_name}({user.id}) is trying to execute function: {func.__name__}."
+            )
+            return await func(*args, **kargs)
+        else:
+            logger.warning(
+                f"User {user.full_name}({user.id}) is trying to execute admin only function: {func.__name__}."
+            )
+            return await args[0].message.reply_text(
+                "You are not permit to do that. Your action will be recorded."
+            )
+    return wraps
+
+def checkVIP(func):
+    async def wraps(*args, **kargs):
+        user = args[0].effective_user
+        
+        if checkIsVIP(user.id):
+            logger.info(
+                f"VIP {user.full_name}({user.id}) is trying to execute function: {func.__name__}."
+            )
+            return await func(*args, **kargs)
+        else:
+            logger.warning(
+                f"User {user.full_name}({user.id}) is trying to execute VIP only function: {func.__name__}."
+            )
+            return await args[0].message.reply_text(
+                "You are not permit to do that. Your action will be recorded."
+            )
+    return wraps
+
+def checkReplied(func):
+    async def wraps(*args, **kargs):
+        replied_message = args[0].message.reply_to_message
+        
+        if replied_message is not None:
+            return await func(*args, **kargs)
+        else:
+            return await args[0].message.reply_text(
+                "This function only works when replying to a specific message. "
+                "Please check again."
+            )
+    return wraps
 
 def HQOnly(func):
     async def wraps(*args, **kargs):
@@ -59,3 +110,18 @@ def GbbFilter(func):
             return await func(*args, **kargs)
         else:
             pass
+    return wraps
+
+def checkArgumentExist(func):
+    async def wraps(*args, **kargs):
+        if args[1].args != []:
+            return await func(*args, **kargs)
+        else:
+            return await args[0].message.reply_text(
+                "This function must operate with arguments. "
+                "Please check again."
+            )
+    return wraps
+
+
+    
